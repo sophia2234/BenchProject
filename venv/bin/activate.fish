28dfa7efe2bf32d1410c70f159f17bbf974c65b3
achen813 <achen813@ad.unc.edu>
2019-07-24 12:55:42 -0400

    Fix Validation for Phoidation for photo type and change password, dashboard updates

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/achen813/myproject/venv"
+set -gx VIRTUAL_ENV "/Users/achen813/BenchProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
